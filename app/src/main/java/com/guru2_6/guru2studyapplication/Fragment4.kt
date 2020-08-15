e2b8f8f7577949c12646ab84b7eba6e2e1408f96
Leejuyeon36 <69339759+Leejuyeon36@users.noreply.github.com>
2020-08-16 01:10:31 +0900

    Update Fragment4.kt

--- app/src/main/java/com/guru2_6/guru2studyapplication/Fragment4.kt
+++ app/src/main/java/com/guru2_6/guru2studyapplication/Fragment4.kt
@@ -1,23 +1,96 @@
 package com.guru2_6.guru2studyapplication
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main4.*
+import java.util.*
+import kotlin.concurrent.timer
 
-class Fragment4 : Fragment(){
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        Log.d("life_cycle","F onCreateView")
-        // fragment가 인터페이스를 처음 그릴때 호출된다
-        // inflater -> 뷰를 그려주는 역할
-        // container -> 부모 뷰
-        return inflater.inflate(R.layout.fragment_test, container, false)
+class Study_StopWatch_Activity : AppCompatActivity() {
+
+    private var time = 0
+    private var timerTask: Timer? = null      // null을 허용
+    private var isRunning = false
+    private var index :Int = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_study__stop_watch_)
+
+        btn_start.setOnClickListener {
+            isRunning = !isRunning
+
+            start()
+        }
+
+        btn_stop.setOnClickListener {
+            stop()
+        }
+
+        btn_reset.setOnClickListener {
+            reset()
+        }
+
+        btn_laptime.setOnClickListener {
+            if(time!=0)
+                laptime()
+        }
     }
+
+    // 시작
+    private fun start() {   //수정
+
+        timerTask = timer(period = 1000) {
+            time++
+
+            val hou = time / 3600
+            val min = time / 60
+            val sec = time % 60
+
+            runOnUiThread { //UI 조작
+                hourView.text = "$hou"
+                minuteView.text = "$min"
+                secondView.text = "$sec"
+            }
+        }
+    }
+
+    // 일시정지
+    private fun stop() {
+        timerTask?.cancel()
+    }
+
+    // 초기화
+    private fun reset() {
+        timerTask?.cancel()
+
+        // 모든 변수 초기화
+        time = 0
+        isRunning = false
+        hourView.text = "00"
+        minuteView.text = "00"
+        secondView.text = "00"
+
+        index = 1
+    }
+
+    // 기록
+    private fun laptime() {
+        val lapTime = time		// 함수 호출 시 시간(time) 저장
+
+        // apply() 스코프 함수로, TextView를 생성과 동시에 초기화
+        val textView = TextView(this).apply {
+            setTextSize(20f)	// fontSize 20 설정
+            text = "${lapTime / 3600}.${lapTime / 60}.${lapTime % 60}"	// 출력할 시간 설정
+        }
+
+        laptimeView.addView(textView,0)	// layout에 추가, (View, index) 추가할 위치(0 최상단 의미)
+        index++	// 추가된 View의 개수를 저장하는 index 변수
+    }
+
+}
+
+private operator fun TextView.invoke(textView: TextView, i: Int) {
 
 }
